b1397456c5780132b1c96d2678bf0e2f2da09c07
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2018-11-25 00:56:08 +0100

    Fix too small temperature trigger in the hunting lodge

---
+++ src/HuntingLodgeTemperatureTriggerFix.cs
@@ -0,0 +1,33 @@
+﻿using Harmony;
+using UnityEngine;
+
+namespace TLD_Bugfixes {
+
+	/*
+	 * In the hunting lodge, there are areas that are a few degrees colder
+	 * than the rest of the building. This is because the InteriorTemperatureTrigger
+	 * was copied & pasted from a Coastal Highway house and the size of its
+	 * collider was not adjusted to the larger hunting lodge scene.
+	 */
+
+	[HarmonyPatch(typeof(GameManager), "Start")]
+	internal static class HuntingLodgeTemperatureTriggerFix {
+
+		private const string TRIGGER_NAME = "/Root/Design/Scripting/TRIGGER_InteriorTemperatureCoastalHouse";
+
+		private static void Postfix(GameManager __instance) {
+			if (__instance.gameObject.scene.name != "HuntingLodgeA")
+				return;
+
+			GameObject interiorTrigger = GameObject.Find(TRIGGER_NAME);
+			BoxCollider collider = interiorTrigger?.GetComponent<BoxCollider>();
+
+			if (!interiorTrigger || !collider) {
+				Debug.LogWarning("[TLD-Bugfixes] Warning: Could not fix the interior temperature in the Hunting Lodge");
+				return;
+			}
+
+			collider.size = new Vector3(100, 100, 100);
+		}
+	}
+}
