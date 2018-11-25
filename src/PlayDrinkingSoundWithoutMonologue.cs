b94247c3dc5163bea885f5be57876905054b197d
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2018-11-25 03:48:12 +0100

    Fix drinking being completely silent when survivor monologue is turned off

---
+++ src/PlayDrinkingSoundWithoutMonologue.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Harmony;
+
+namespace TLD_Bugfixes {
+
+	/*
+	 * When "Survivor Monologue" is turned off in a custom mode game, drinking is
+	 * completely silent, whereas eating still creates some sound.
+	 *
+	 * This might just be because the order of arguments has been mixed up when calling Launch,
+	 * or maybe this was a deliberate (albeit weird) design decision. Neither the eating
+	 * nor the drinking noises constitute "Survivor Monologue" in my opinion.
+	 *
+	 * See TLDP-6468.
+	 */
+
+	[HarmonyPatch(typeof(PlayerManager), "DrinkFromWaterSupply")]
+	internal static class PlayDrinkingSoundWithoutMonologue {
+		private static readonly MethodBase target = AccessTools.Method(typeof(Panel_GenericProgressBar), "Launch");
+		private static readonly MethodBase replacement = AccessTools.Method(typeof(PlayDrinkingSoundWithoutMonologue), "FixArgsOrder");
+
+		private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+			return Transpilers.MethodReplacer(instructions, target, replacement);
+		}
+
+		private static void FixArgsOrder(Panel_GenericProgressBar progressBarPanel,
+		                                 string name, float seconds, float minutes, float randomFailureThreshold,
+		                                 string audioNameShouldBeVoiceName, string voiceNameShouldBeAudioName,
+		                                 bool supressHeavyBreathing, bool skipRestoreInHands, OnExitDelegate del) {
+
+			progressBarPanel.Launch(name, seconds, minutes, randomFailureThreshold,
+			                        voiceNameShouldBeAudioName, audioNameShouldBeVoiceName,
+			                        supressHeavyBreathing, skipRestoreInHands, del);
+		}
+	}
+}
