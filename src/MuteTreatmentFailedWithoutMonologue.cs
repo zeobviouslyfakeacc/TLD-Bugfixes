16b299e02f97fd417434c93f6fda00d2d9fead0f
zeobviouslyfakeacc <zeobviouslyfakeacc@users.noreply.github.com>
2018-11-25 17:48:31 +0100

    Don't complain about a treatment not working when survivor monologue is turned off

---
+++ src/MuteTreatmentFailedWithoutMonologue.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Harmony;
+using UnityEngine;
+
+namespace TLD_Bugfixes {
+
+	/*
+	 * When "Survivor Monologue" is turned off in a custom mode game, the player
+	 * still monologues about a treatment not working, e.g. when drinking medicinal
+	 * tea without having any afflictions.
+	 *
+	 * See TLDP-6339.
+	 */
+
+	[HarmonyPatch(typeof(PlayerManager), "OnFirstAidComplete")]
+	internal static class MuteTreatmentFailedWithoutMonologue {
+		private static readonly MethodBase target = AccessTools.Method(typeof(GameAudioManager), "PlaySound",
+				new Type[] { typeof(string), typeof(GameObject) });
+		private static readonly MethodBase replacement = AccessTools.Method(typeof(MuteTreatmentFailedWithoutMonologue), "PlayVoice");
+
+		private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+			return Transpilers.MethodReplacer(instructions, target, replacement);
+		}
+
+		private static uint PlayVoice(string soundID, GameObject goIgnored) {
+			return GameManager.GetPlayerVoiceComponent().PlayCritical(soundID);
+		}
+	}
+}
